2ced6cf79e44187b2db8e85c1c5d0465e92046c6
sam <sampurna.chapagainnn@gmail.com>
2021-11-25 23:29:07 +0545

    use fetch api to make toggle work for db.json file

--- src/components/ExerciseItems.js
+++ src/components/ExerciseItems.js
@@ -10,7 +10,15 @@ const ExerciseItem = ({exercise, performDelete, performToggle}) => {
   }
 
   const handleToggle=()=>{
+    fetch(`http://localhost:3001/exercises/${exercise.id}`,{
+      method: 'PATCH',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({complete: !exercise.complete})   
+    })
+    .then(()=> {
     performToggle(exercise.id)
+    })
+    .catch((error)=>console.log(error));
     console.log(exercise.complete)
   }
 
